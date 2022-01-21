3bdbef160c1fad6fcc1163290db2ccf2f543d5a5
Yaroslav Akulov <akulovYa@Yandex.ru>
2022-01-21 12:27:00 +0700

    Initial Commit

---
+++ GuessTheNumberApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheNumberApp
+//
+//  Created by Ярослав Акулов on 21.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
